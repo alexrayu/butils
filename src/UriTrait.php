2fb295460b01731be55476b130a6527955390d31
Alexei Raiu <alex.rayu@gmail.com>
2020-06-24 08:44:56 +0300

    Add UriTrait.php

---
+++ src/UriTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\butils;
+
+use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
+
+/**
+ * Trait Html.
+ *
+ * Html related utils.
+ */
+trait UriTrait {
+
+  /**
+   * Get string from URL respecting language aliases.
+   *
+   * @param string $uri
+   *   Drupal uri.
+   * @param array $options
+   *   Url options.
+   */
+  public function uriToString($uri, array $options = []) {
+    $uri_parts = parse_url($uri);
+    $string = '';
+    if ($uri_parts['scheme'] === 'entity') {
+      $parts = explode('/', $uri_parts['path']);
+      if ($parts[0] === 'node' && !empty($parts[1])) {
+        $node = Node::load($parts[1]);
+        if (!empty($node)) {
+          $string = $node->toUrl()->setAbsolute()->toString();
+        }
+      }
+    }
+    if (empty($string)) {
+      $string = Url::fromUri($uri, $options)->toString();
+    }
+
+    return $string;
+  }
+
+}
